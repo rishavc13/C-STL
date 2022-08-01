ac48e3ffaeba13d7d2967d901acab64cf5cf9044
Rishav <rishav@tredence.com>
2022-08-01 22:02:48 +0530

    added array and vector codes

---
+++ Vector.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+
+using namespacestd;
+
+int main() {
+    vector<int> v;
+
+    vector<int> a(5,1); // all elements initialized to 1
+    cout<<"Printing a :"<<endl;
+    for(int i:a)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    vector<int> last(a); // copy elements of vector a into last vector
+    cout<<"Printing last :"<<endl;
+    for(int i:last)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Capacity ="<<v.capacity()<<endl;
+    
+    v.push_back(1);
+    cout<<"Capacity ="<<v.capacity()<<endl;
+
+    v.push_back(2);
+    cout<<"Capacity ="<<v.capacity()<<endl;
+
+    v.push_back(3);
+    cout<<"Capacity ="<<v.capacity()<<endl;
+    cout<<"Size ="<<v.size()<<endl;
+
+    cout<<"Element at second index ="<<v.at(2)<<endl;
+    cout<<"Front element ="<<v.front()<<endl;
+    cout<<"Last element ="<<v.back()<<endl;
+
+    cout<<"Before pop_back :"<<endl;
+    for(int i:v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl<<"After Pop :"<<endl;
+    v.pop_back();
+
+    for(int 1:v)
+    {
+        cout<<i<<" ";
+    }
+    
+    cout<<"Before clear size "<<v.size()<<endl;
+    v.clear();
+    cout<<"after clear size "<<v.size()<<endl;
+}
